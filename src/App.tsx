d8f001f2ca73a8ef5abd90d2ffed503b0fcd6ccc
Fla Monteiro <flaviomonteiro2013@gmail.com>
2024-07-21 18:08:12 -0300

    iniciar conversão para tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,23 @@
+import { ThemeProvider } from 'styled-components'
 import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
+
+import { defaultTheme } from './themes/default'
+import GlobalStyle from './styles/global'
 
 import './global.css'
 
 function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
+      <GlobalStyle />
       <Header />
       <Hero />
       <div className="container">
         <ListaVagas />
       </div>
-    </>
+    </ThemeProvider>
   )
 }
 
