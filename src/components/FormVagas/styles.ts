374e7a76d95cda2def52f58afcac73195b162d74
Fla Monteiro <flaviomonteiro2013@gmail.com>
2024-07-21 21:56:00 -0300

    FormVagas para styled-components

---
+++ src/components/FormVagas/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components'
+
+export const StyledForm = styled.form`
+  display: grid;
+  grid-template-columns: 1fr auto;
+  background-color: ${(props) => props.theme.corSecundaria};
+  padding: 32px;
+  border-radius: 12px;
+  margin-top: 40px;
+`
+
+export const StyledInput = styled.input`
+  padding: 0 16px;
+  outline-color: ${(props) => props.theme.corSecundaria};
+`
+
+export const StyledButton = styled.button`
+  background-color: ${(props) => props.theme.corSecundaria};
+  border: 1px solid ${(props) => props.theme.corSecundaria};
+  height: 40px;
+  padding: 0 16px;
+  font-size: 18px;
+  color: ${(props) => props.theme.corSecundaria};
+  margin-left: 8px;
+  cursor: pointer;
+`
